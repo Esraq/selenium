ca267e0ffa25f61a9f7feb17d86fb4bff67f77e1
Esraq Humayun Eisty <33447246+Esraq@users.noreply.github.com>
2021-08-15 17:37:29 +0600

    First test

---
+++ SeleniumFirst/SeleniumFirst/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create the reference for the browser
+            IWebDriver driver = new FirefoxDriver();
+
+            //Navigate to google page 
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            ///find the element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //perform ops
+            element.SendKeys("executeautomation");
+
+            ///driver.Close();
+
+        }
+
+
+    }
+
+        
+    }
+
